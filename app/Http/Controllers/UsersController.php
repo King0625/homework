8663bf312338e609217a3e2562da0d4abd43671d
King0625 <a7164265@gmail.com>
2019-08-16 10:07:14 +0800

    Create store method on UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
@@ -16,9 +18,21 @@ class UsersController extends Controller
         //
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required|min:2|max:256',
+            'email' => 'required|email|max:256|unique:users',
+            'password' => 'required|min:6|max:12'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            return response()->json(['message' => 'Request error!!'], 400);
+        }
+
+        $user = User::create($request->all());
+        return response()->json($user, 201);
     }
 
     public function update()
