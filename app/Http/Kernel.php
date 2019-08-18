12b99efec7744661c38937f8aa6bb3e652d375bc
King0625 <a7164265@gmail.com>
2019-08-18 22:16:12 +0800

    Register basic.auth middleware in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user.auth' => \App\Http\Middleware\UserAuth::class,
+        'basic.auth' => \App\Http\Middleware\BasicAuth::class,
     ];
 
     /**
